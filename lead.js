17c64a36248c83ddf75458af68b15391bdc0b5b2
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-02-26 16:44:34 -0600

    added jquery for server response

--- lead.js
+++ lead.js
@@ -18,24 +18,24 @@ $(document).ready(function(){
 });
 
 // ------------------Validations: First, Last, Email------------
-$(document).ready(function(){
-  $('#lead_form').submit(function() {
-    $('div#error_message').empty();
-    $('div#success_message').empty();
-    var first = document.getElementById("first");
-    var last = document.getElementById("last");
-    var email = document.getElementById("email");
-    alert("document.getElementById('first').value " + first);
-    if (first.value.length > 255 || first.value == "") {
-      $('div#error_message').append("--Enter a valid first name.<br>");
-      event.preventDefault();
-    } 
-    if (last.value.length > 255 || last.value == "" ) {
-      $('div#error_message').append("--Enter a valid last name.<br>");
-      event.preventDefault();
-    }
-    if ($('div#error_message').html() == "") {
-      $('div#success_message').append("--Success!<br>");
-    };
-  });
-});
+// $(document).ready(function(){
+//   $('#lead_form').submit(function() {
+//     $('div#error_message').empty();
+//     $('div#success_message').empty();
+//     var first = document.getElementById("first");
+//     var last = document.getElementById("last");
+//     var email = document.getElementById("email");
+//     alert("document.getElementById('first').value " + first);
+//     if (first.value.length > 255 || first.value == "") {
+//       $('div#error_message').append("--Enter a valid first name.<br>");
+//       event.preventDefault();
+//     } 
+//     if (last.value.length > 255 || last.value == "" ) {
+//       $('div#error_message').append("--Enter a valid last name.<br>");
+//       event.preventDefault();
+//     }
+//     if ($('div#error_message').html() == "") {
+//       $('div#success_message').append("--Success!<br>");
+//     };
+//   });
+// });
--- response-msg.js
+++ response-msg.js
@@ -1,1 +1,39 @@
-response-msg.js
+// Pasrses the server response:
+$(document).ready(function(){
+  (function() {
+    var httpRequest;
+    document.getElementById("#submit-button").onclick = function() { makeRequest('index.html'); };
+
+    function makeRequest(url) {
+      if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+        httpRequest = new XMLHttpRequest();
+      } else if (window.ActiveXObject) { // IE
+        try {
+        httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+        } 
+        catch (e) {
+          try {
+            httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+          } 
+          catch (e) {}
+        }
+      }
+      httpRequest.onreadystatechange = alertContents;
+      httpRequest.open('GET', url);
+      httpRequest.send();
+    }
+
+    function alertContents() {
+      if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+          alert(httpRequest.responseText);
+        } else {
+          alert('There was a problem with the request.');
+        }
+      }
+    }
+
+  var response = $("response").html();
+  alert("html(): " + response);
+  });
+});
